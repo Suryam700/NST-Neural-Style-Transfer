4e3e0ee9f07fb9417426a76e9f8b2b86b5e66e5a
Suryam Gahoi <suryamgahoi14@gmail.com>
2026-08-06 18:28:09 +0530

    add dataset and dataloader

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import argparse
 import torch
 from pathlib import Path
+from utils.utils import *
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -9,7 +11,10 @@ def parse_arguments():
     parser.add_argument('--style_dir', type=str, default=r'C:\users\rits\Documents\NST-Neural-Style-Transfer\style_data', help='Location of style dataset')
     parser.add_argument('--vgg', type=str, default=r'C:\users\rits\Documents\NST-Neural-Style-Transfer\vgg_normalised.pth', help='Location of pre-trained VGG')
     parser.add_argument('--experiment', type=str, default='experiment1', help='Name of experiment')
-
+    parser.add_argument('--final_size', type=int, default=512, help='Size of final image')
+    parser.add_argument('--content_size', type=int, default=256, help='Size of content image')
+    parser.add_argument('--style_size', type=int, default=256, help='Size of style image')
+    parser.add_argument('--crop', action='store_true', default=True, help='Crop Image')
 
     return parser.parse_args()
 
@@ -25,6 +30,16 @@ def main():
     with open(save_dir/'args.txt', 'w') as args_file:
         for key, val in vars(args).items():
             args_file.write(f"{key}: {val}\n")
+
+    content_transform = get_transform(args.content_size, args.final_size, args.crop)
+    style_transform = get_transform(args.style_size, args.final_size, args.crop)
+
+    content_dataset = ImageFolderDataset(args.content_dir, content_transform)
+    style_dataset = ImageFolderDataset(args.style_dir, style_transform)
+
+    content_dataloader = DataLoader(content_dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True, drop_last=True)
+    style_dataloader = DataLoader(style_dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True, drop_last=True)
+
     
 
 if __name__ == "__main__":
---
+++ utils/utils.py
@@ -0,0 +1,38 @@
+from torch.utils.data import Dataset, DataLoader
+import os
+from PIL import Image
+from torchvision import transforms
+
+class ImageFolderDataset(Dataset):
+    def __init__(self, root, transform = None):
+        super(ImageFolderDataset, self).__init__()
+        self.root = root
+        self.transform = transform
+        self.files = [p for p in list(os.listdir(root)) if p.endswith('.jpg', '.jpeg', '.png')]
+
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root, self.files[index])
+
+        if self.transform:
+            image = self.transform(Image.open(img_path))
+        try:
+            return image
+        except Exception as e:
+            raise RuntimeError(f"Failed to load image: {img_path}") from e
+
+
+def get_transform(size, final_size, crop):
+    transform_list = []
+
+    if size > 0: transform_list.append(transforms.Resize(size))
+    if crop: 
+        transform_list.append(transforms.RandomCrop(final_size)) 
+    else: 
+        transform_list.append(transforms.Resize(final_size))
+
+    transform_list.append(transforms.ToTensor())
+    return transforms.Compose(transform_list)
