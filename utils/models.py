a505e87432936aa5377ecb744d3bd8cc20f999c8
Suryam Gahoi <suryamgahoi14@gmail.com>
2026-08-11 22:27:23 +0530

    implement encoder and decoder for VGG

--- utils/models.py
+++ utils/models.py
@@ -113,7 +113,7 @@ class Decoder(nn.Module):
             nn.Conv2d(64, 64, (3, 3)),
             nn.ReLU(),
             nn.ReflectionPad2d((1, 1, 1, 1)),
-            nn.Conv2d(64, 3, (3, 3)),         
+            nn.Conv2d(64, 3, (3, 3))  
         )
 
     def forward(self, input):
